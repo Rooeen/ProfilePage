6d0c332a0d13f9d9f2fbfe14e17f226ab98a523c
Rubeen <93068014+Rooeen@users.noreply.github.com>
2022-12-25 12:42:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
  </nav>
 </header>
 <div id="center">
-<div id="center-set"> - CLOUD STARLETS
+<div id="center-set"> - Rubeen Noolkar
 <div id="signup">
 <div id="signup-st">
 <div align="center">
